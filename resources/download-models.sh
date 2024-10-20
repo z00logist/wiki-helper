8ad351758c319021cd18b84d3baab90d84c798d2
Anastasia <a.margolina@chatme.ai>
2024-10-20 17:09:02 +0200

    change model to gemma

--- resources/download-models.sh
+++ resources/download-models.sh
@@ -1,3 +1,3 @@
 huggingface-cli download mixedbread-ai/mxbai-embed-large-v1 --local-dir ./models/embedder/
 
-huggingface-cli download  unsloth/Llama-3.2-3B-Instruct-GGUF --include "Llama-3.2-3B-Instruct-Q6_K.gguf" --local-dir ./models/generative_model/
+huggingface-cli download  bartowski/gemma-2-2b-it-GGUF  --include "gemma-2-2b-it-Q6_K.gguf" --local-dir ./models/generative_model/
